98956eb2bfcbdb7bf17eaf5a4f5f987595bc9330
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-07-01 13:49:23 -0400

    Update build.pl

--- build.pl
+++ build.pl
@@ -1,12 +1,13 @@
-use Data::Dumper;
-$Data::Dumper::Terse = 1;
-$Data::Dumper::Deparse = 1;
+#!/usr/bin/perl
 
+# | We expect a source directory to exist.
 opendir DIR, 'src'
     or die 'no /src';
 
-@files = ();
-        
+# | Will store our list of c files to compile.
+my @files = ();
+
+# | Find all C source files and add them to the list of files.
 for (;;) {
     my $file = readdir DIR;
     
@@ -17,20 +18,26 @@ for (;;) {
     }
 }
 
+# | Create an 'obj' directory if one does not already exist.
 mkdir 'obj' unless -d 'obj';
 
+# | Compile each .c file in /src to a corresponding .obj file in /obj.
 foreach(@files) {
     `gcc src/$_.c -o obj/$_.obj -c`;
 }
 
+# | Convert our file names list into full paths to object files.
 for (my $i = 0; $i < scalar @files; ++$i) {
     $files[$i] = 'obj/' . $files[$i] . '.obj';
 }
 
+# | Create a lib directory unless one already exists.
 mkdir 'lib' unless -d 'lib';
 
-$s = join ' ', @files;
+# | A string containing all the object file paths delimited by space.
+my $s = join ' ', @files;
 
+# | Compile our collection of object files into a single statically linked library.
 `ar rcs lib/libcdcmatrix.a $s`;
 
 
