636c0edc058b2e5cb6b2985a7f9ca4becac5735a
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-07-01 12:59:31 -0400

    Update matrix_transposef.c

--- src/matrix_transposef.c
+++ src/matrix_transposef.c
@@ -1,9 +1,11 @@
-void matrix_transposef(void *Out, void *In, int Rows, int ColumnsPerRow)
+#include <stdint.h>
+
+void matrix_transposef(void *Out, void *In, uint32_t Rows, uint32_t ColumnsPerRow)
 {
     #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) (__RowLength * __RowIndex + __ColumnIndex)
 
-    int i;
-    int j;
+    uint_fast32_t i;
+    uint_fast32_t j;
     
     for (i = 0; i < Rows; ++i) {
         for (j = 0; j < ColumnsPerRow; ++j) {
