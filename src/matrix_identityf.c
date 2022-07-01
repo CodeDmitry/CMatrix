33a6013713bf00349a35bc53c0f2732950a16c88
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-06-30 23:43:37 -0400

    Update matrix_identityf.c

--- src/matrix_identityf.c
+++ src/matrix_identityf.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 
 /**
- * Fills a Size*Size float-compatible matrix with Coefficient on each element of the main diagonal.
+ * Fills a Size*Size float-compatible matrix with Coefficient in each 
+ *     element of the main diagonal.
  * 
  * This works both for float[Size][Size] and float[Size] or any 
  *     binary-compatible struct or class or union containing such.
@@ -16,12 +17,14 @@
  *     matrix_identityf(m1, 3, 7);
  *
  * @param Out a pointer where to place the identity matrix requested.
- * @param Size indicates the number of both rows and columns for this identity matrix.
- * @param Coefficient indicates the coefficient to place on the main diagonal of the matrix.
+ * @param Size indicates the number of both rows and columns for this 
+ *     identity matrix.
+ * @param Coefficient indicates the coefficient to place on the 
+ *     main diagonal of the matrix.
  */
 void matrix_identityf(void *Out, uint32_t Size, float Coefficient)
 {
-    /** given row size and indices, compute flat offset into the matrix. */ 
+    // | Given row size and indices, compute flat offset into the matrix. 
     #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) \
         (__RowLength * __RowIndex + __ColumnIndex)
 
@@ -33,18 +36,19 @@ void matrix_identityf(void *Out, uint32_t Size, float Coefficient)
         for (j = 0; j < Size; ++j) {
             float val;
             
-            /** If we are on the main diagonal, use the Coefficient, otherwise use 0. */
+            // | If we are on the main diagonal, use the Coefficient, 
+            // | otherwise use 0. 
             if (i == j) {
                 val = Coefficient;
             } else {
                 val = 0;
             }
             
-            /** Fill the elements of the matrix. */
+            // | Fill the elements of the matrix. 
             lpOut[__MATRIX_INDEX(Size, i, j)] = val;
         }
     }
     
-    /** limit lifetime of the macro to this scope. */
+    // | limit lifetime of the macro to this scope. 
     #undef __MATRIX_INDEX
 }
