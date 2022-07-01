f0288c5973b17e9835e460b3d88153e58ec925ef
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-07-01 12:29:40 -0400

    Update matrix_addf.c

--- src/matrix_addf.c
+++ src/matrix_addf.c
@@ -1,11 +1,28 @@
 #include <stdint.h>
 
+/**
+ * Adds two matrices A and B and places the result into Out.
+ *
+ * Usage Examples:
+ *     float msum[16];
+ *     float m1[16];
+ *     float m2[16];
+ *     matrix_identityf(m1, 4, 1);
+ *     matrix_identityf(m2, 4, 2);
+ *     matrix_addf(msum, m1, m2, 4, 4);
+ *
+ * @param Out A pointer where to place the sum.
+ * @param A The first matrix.
+ * @param B The second matrix.
+ * @param NumberOfRows The number of rows of given matrices.
+ * @param ColumnsPerRow The column count of given matrices.
+ */
 void matrix_addf (
     void *Out, 
     void *A, 
     void *B, 
     uint32_t NumberOfRows, 
-    uint32_t ColumnsPerRow)
+    uint32_t ColumnsPerRow )
 {    
     uint32_t i;
     
