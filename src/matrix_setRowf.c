950a330b330b7cf831dda458e5895411454f696e
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-11 17:45:54 -0400

    Update matrix_setRowf.c

--- src/matrix_setRowf.c
+++ src/matrix_setRowf.c
@@ -6,16 +6,12 @@ void matrix_setRowf (
     uint32_t RowIndex,
     uint32_t ColumnsPerRow)
 {
-    #define __MATRIX_ROWSTARTOFFSET(ColsPerRow, RowIndex) (ColsPerRow * RowIndex)
-
     uint32_t rowStartIndex;
     uint32_t i;
     
-    rowStartIndex = __MATRIX_ROWSTARTOFFSET(ColumnsPerRow, RowIndex);    
+    rowStartIndex = ColumnsPerRow * RowIndex;    
     
     for (i = 0; i < 4; ++i) {
         ((float *)Out)[rowStartIndex + i] = ((float *)In)[i];
     }
-    
-    #undef __MATRIX_ROWSTARTOFFSET
 }
