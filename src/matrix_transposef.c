994ab8b43254b8ede46b0400977a0355d9d5338b
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-11 17:35:13 -0400

    Update matrix_transposef.c
    Made the code fit on an 80 character terminal.

--- src/matrix_transposef.c
+++ src/matrix_transposef.c
@@ -1,15 +1,22 @@
 #include <stdint.h>
 
-void matrix_transposef(void *Out, void *In, uint32_t Rows, uint32_t ColumnsPerRow)
+void matrix_transposef (
+    void *Out, 
+    void *In, 
+    uint32_t Rows, 
+    uint32_t ColumnsPerRow )
 {
-    #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) (__RowLength * __RowIndex + __ColumnIndex)
+    #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) \
+        (__RowLength * __RowIndex + __ColumnIndex)
 
     uint_fast32_t i;
     uint_fast32_t j;
     
     for (i = 0; i < Rows; ++i) {
         for (j = 0; j < ColumnsPerRow; ++j) {
-            ((float *)Out)[__MATRIX_INDEX(ColumnsPerRow, i, j)] = ((float *)In)[__MATRIX_INDEX(Rows, j, i)];
+            ((float *)Out)[__MATRIX_INDEX(ColumnsPerRow, i, j)] 
+                = ((float *)In)[__MATRIX_INDEX(Rows, j, i)]
+                ;
         }
     }
     
