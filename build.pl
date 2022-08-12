8b099ea6fd6152e8996852a4c9708252a68b922a
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-12 04:50:38 -0400

    Update build.pl
    Slight readability improvement.

--- build.pl
+++ build.pl
@@ -1,43 +1,62 @@
 #!/usr/bin/perl
 
-# | We expect a source directory to exist.
-opendir DIR, 'src'
-    or die 'no /src';
+# | Attempt to open the 'src' directory as DIR, 
+# |     terminate if we cannot find it or 
+# |     if we cannot open it.
+my $open_source_dir_result = opendir(DIR, 'src');
+if ($open_source_dir_result == 0) {
+    die('The /src directory cannot be opened or does not exist.');
+}
 
-# | Will store our list of c files to compile.
+# | List of C file names to compile.
 my @files = ();
 
 # | Find all C source files and add them to the list of files.
+# TODO: deobfuscate, section has excessive reading complexity.
 for (;;) {
-    my $file = readdir DIR;
+    # | readdir returns undef if we have finished reading all the files.
+    my $fileName = readdir(DIR);
     
-    last unless $file;
+    # | if $fileName is undef, we are done making our list.
+    last unless $fileName;
     
-    if (!($file =~ /^[.]/) && $file =~ /[.]c$/) {    
-        push @files, (substr $file, 0, -2);
+    # | Make sure we only add the file to the list
+    # |     if it is a c file. 
+    if (!($fileName =~ /^[.]/) && $fileName =~ /[.]c$/) {    
+        my $filenameWithoutExtension = substr($fileName, 0, -2); 
+        push @files, $filenameWithoutExtension;
     }
 }
 
-# | Create an 'obj' directory if one does not already exist.
-mkdir 'obj' unless -d 'obj';
+# done using 
+closedir(DIR);
 
-# | Compile each .c file in /src to a corresponding .obj file in /obj.
+# Create an 'obj' directory unless it already exists.
+if (!(-d 'obj')) {
+    mkdir('obj');
+}
+
+# | Use gcc to compile
+# |     each .c file in '/src' to a corresponding '.obj' file in '/obj'.
 foreach(@files) {
     `gcc src/$_.c -o obj/$_.obj -c`;
 }
 
 # | Convert our file names list into full paths to object files.
-for (my $i = 0; $i < scalar @files; ++$i) {
+for (my $i = 0; $i < scalar(@files); ++$i) {
     $files[$i] = 'obj/' . $files[$i] . '.obj';
 }
 
-# | Create a lib directory unless one already exists.
-mkdir 'lib' unless -d 'lib';
+# | Create a 'lib' directory unless it already exists.
+if (!(-d 'lib')) {
+    mkdir('lib') 
+}
 
 # | A string containing all the object file paths delimited by space.
-my $s = join ' ', @files;
+my $s = join(' ', @files);
 
-# | Compile our collection of object files into a single statically linked library.
+# | Compile our collection of object files into a 
+# |     static library.
 `ar rcs lib/libcdcmatrix.a $s`;
 
 
