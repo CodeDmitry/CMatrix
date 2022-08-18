28ed3bf298805442f21f581d5902a5782d82d08c
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-18 19:41:18 -0400

    Update matrix_transposef.c

--- src/matrix_transposef.c
+++ src/matrix_transposef.c
@@ -1,21 +1,31 @@
 #include <stdint.h>
 
 void matrix_transposef (
-    void *Out, 
-    void *In, 
-    uint32_t Rows, 
-    uint32_t ColumnsPerRow )
+    /* out */ void *pvResultMatrix, 
+    /* in */ void *pvGivenMatrix, 
+    /* in */ uint32_t Rows, 
+    /* in */ uint32_t ColumnsPerRow )
 {
-    #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) \
-        (__RowLength * __RowIndex + __ColumnIndex)
+    // | A temporary macro to make indexing more explicit,
+    // |     potentially better to use pointers to 2d.
+    // | rowlen: Row Length.
+    // | rowidx: Row Index.
+    // | colidx: Column Index.
+    // | result: offset units to get [rowidx][colidx].
+    #define __MATRIX_INDEX(rowlen, rowidx, colidx) \
+        (rowlen*rowidx + colidx)
 
     uint_fast32_t i;
     uint_fast32_t j;
+    float *rm = (float *)pvResultMatrix;
+    float *gm = (float *)pvGivenMatrix;
+    uint32_t nr = Rows;
+    uint32_t nc = ColumnsPerRow;
     
-    for (i = 0; i < Rows; ++i) {
-        for (j = 0; j < ColumnsPerRow; ++j) {
-            ((float *)Out)[__MATRIX_INDEX(ColumnsPerRow, i, j)] 
-                = ((float *)In)[__MATRIX_INDEX(Rows, j, i)]
+    for (i = 0; i < nr; ++i) {
+        for (j = 0; j < nc; ++j) {
+            rm[__MATRIX_INDEX(nc, i, j)] 
+                = gm[__MATRIX_INDEX(nr, j, i)]
                 ;
         }
     }
