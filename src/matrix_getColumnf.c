bfde2181a82679480a6fdb6822ee96c372c0fa61
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-18 20:52:38 -0400

    Update matrix_getColumnf.c

--- src/matrix_getColumnf.c
+++ src/matrix_getColumnf.c
@@ -1,19 +1,34 @@
 #include <stdint.h>
 
 void matrix_getColumnf (
-    void *Out,
-    void *In,
-    uint32_t ColumnIndex,
-    uint32_t NumberOfRows,
-    uint32_t ColumnsPerRow)
+    /* out notnull */ void *pvResultMatrix,
+    /* in notnull */ void *pvGivenMatrix,
+    /* in */ uint32_t ColumnIndex,
+    /* in */ uint32_t NumberOfRows,
+    /* in */ uint32_t ColumnsPerRow )
 {
-    #define __MATRIX_INDEX(__RowLength, __RowIndex, __ColumnIndex) (__RowLength * __RowIndex + __ColumnIndex)
-
+    // | A temporary macro to get flat index of the 
+    // |     element at [rowidx][colidx], given
+    // |     a specified row length(rowlen).
+    // | rowlen: Row Length.
+    // | rowidx: Row Index.
+    // | colidx: Column Index.
+    // | result: offset units to get [rowidx][colidx].
+    #define matidx(rowlen, rowidx, colidx) \
+        (rowlen*rowidx + colidx)
+    
+    float *rm = (float *)pvResultMatrix;
+    float *gm = (float *)pvGivenMatrix;
+    uint32_t ci = ColumnIndex;
+    uint32_t nr = NumberOfRows;
+    uint32_t nc = ColumnsPerRow;
     uint32_t i;
     
-    for (i = 0; i < NumberOfRows; ++i) {        
-        ((float *)Out)[i] = ((float *)In)[__MATRIX_INDEX(ColumnsPerRow, i, ColumnIndex)];
+    for (i = 0; i < nr; ++i) {        
+        rm[i] = gm[matidx(nc, i, ci)];
     }
     
-    #undef __MATRIX_INDEX
+    // | Release the macro, as this file might
+    // |     get cut/pasted into another file.
+    #undef matidx
 }
