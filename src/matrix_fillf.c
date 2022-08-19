287e08930cec2346624776c0adedc34692bf386a
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-18 20:38:12 -0400

    Update matrix_fillf.c

--- src/matrix_fillf.c
+++ src/matrix_fillf.c
@@ -1,14 +1,18 @@
 #include <stdint.h>
 
-void matrix_fillf(
-    void *Out, 
-    float Value, 
-    uint32_t Rows, 
-    uint32_t Columns) 
+void matrix_fillf (
+    /* out */ void *pvResultMatrix, 
+    /* in */ float fFillValue, 
+    /* in */ uint32_t NumberOfRows, 
+    /* in */ uint32_t ColumnsPerRow ) 
 {    
+    float *rm = (float *)pvResultMatrix;    
+    float fv = fFillValue;
+    uint32_t nr = NumberOfRows;
+    uint32_t nc = ColumnsPerRow;
     uint32_t i;
 
-    for (i = 0; i < Rows * Columns; ++i) {
-        ((float *)Out)[i] = Value;
+    for (i = 0; i < nr*nc; ++i) {
+        rm[i] = fv;
     }
 }
