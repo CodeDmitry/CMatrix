492f0b095ca3f54227186a30d2bb77cd9af0aaad
CodeDmitry <3159365+CodeDmitry@users.noreply.github.com>
2022-08-18 20:44:50 -0400

    Update matrix.h

--- include/matrix/matrix.h
+++ include/matrix/matrix.h
@@ -26,8 +26,8 @@
  *     - ColumnsPerRow must be correct.
  */
 extern void matrix_dumpf (
-    /* in */ void *Stream, 
-    /* in */ void *Matrix, 
+    /* in notnull */ void *Stream, 
+    /* in notnull */ void *Matrix, 
     /* in */ uint32_t NumberOfRows, 
     /* in */ uint32_t ColumnsPerRow );
 
@@ -42,67 +42,67 @@ extern void matrix_dumpf (
  *     - ColumnsPerRow must be correct.
  */
 extern void matrix_identityf (
-    /* out */ void *Out, 
+    /* out notnull */ void *Out, 
     /* in */ uint32_t Size, 
     /* in */ float Coefficient );
     
 /* Out(r1 by c2) = A(r1 by r1c2) * B(c1r2 by c2). */
 extern void matrix_multiplyf (
-    /* out */ void *Out, 
-    /* in */ void *A, 
-    /* in */ void *B, 
+    /* out notnull */ void *Out, 
+    /* in notnull */ void *A, 
+    /* in notnull */ void *B, 
     /* in */ uint32_t r1, 
     /* in */ uint32_t c1r2, 
     /* in */ uint32_t c2 );    
 
 /* sets Out to the sum of In1 and In2. */    
 extern void matrix_addf (
-    /* out */ void *Out, 
-    /* in */ void *In1, 
-    /* in */ void *In2, 
+    /* out notnull */ void *Out, 
+    /* in notnull */ void *In1, 
+    /* in notnull */ void *In2, 
     /* in */ uint32_t NumberOfRows, 
     /* in */ uint32_t ColumnsPerRow );
     
 /* copies column ColumnIndex of In to Out. */
 extern void matrix_getColumnf (
-    /* out */ void *Out,
-    /* in */ void *In,
+    /* out notnull */ void *Out,
+    /* in notnull */ void *In,
     /* in */ uint32_t ColumnIndex,
     /* in */ uint32_t NumberOfRows,
     /* in */ uint32_t ColumnsPerRow );
 
 /* copies row RowIndex of In to Out. */
 extern void matrix_getRowf (
-    /* out */ void *Out,
-    /* in */ void *In,
+    /* out notnull */ void *Out,
+    /* in notnull */ void *In,
     /* in */ uint32_t RowIndex,
     /* in */ uint32_t ColumnsPerRow );
 
 /* copies In with every every slot scaled by Coefficient into Out. */
 extern void matrix_multiply_scalarf (
-    /* out */ void *Out, 
-    /* in */ void *In,
+    /* out notnull */ void *Out, 
+    /* in notnull */ void *In,
     /* in */ void *Coefficient,
     /* in */ uint32_t NumberOfRows,
     /* in */ uint32_t ColumnsPerRow);
 
 /* fills every slot of Target with Value. */   
 extern void matrix_fillf (
-    /* in out */ void *Target, 
-    /* in */ float Value, 
+    /* in out notnull */ void *Target, 
+    /* in notnull */ float Value, 
     /* in */ uint32_t NumberOfRows, 
     /* in */ uint32_t ColumnsPerRow ); 
 
 /* scales row RowIndex of Target by Coefficient. */
 extern void matrix_scaleRowf ( 
-    /* in out */ void *Target, 
+    /* in out notnull */ void *Target, 
     /* in */ uint32_t ColumnsPerRow, 
     /* in */ uint32_t RowIndex, 
     /* in */ float Coefficient );
 
 /* scales column ColumnIndex of Target by Coefficient. */
 extern void matrix_scaleColumnf (
-    /* in out */ void *Target, 
+    /* in out notnull */ void *Target, 
     /* in */ uint32_t NumberOfRows,
     /* in */ uint32_t ColumnsPerRow, 
     /* in */ uint32_t ColumnIndex, 
@@ -111,15 +111,15 @@ extern void matrix_scaleColumnf (
 /* puts a 2x2 rotaton matrix with angle degrees into Out.
 set flags to 1 to use radians instead. */
 extern void matrix2_rotationf (
-    /* out */ void *Out,
+    /* out notnull */ void *Out,
     /* in */ float angle,
     /* in */ uint32_t flags );
     
 /* puts a 3x3 rotaton matrix with xangle,yangle,zangle degrees into Out
 set flags to 1 to use radians instead. */
 extern void matrix3_rotationf (
-    /* out */ void *Out,
-    /* in */ float xangle,
+    /* out notnull */ void *Out,
+    /* in notnull */ float xangle,
     /* in */ float yangle,
     /* in */ float zangle,
     /* in opt */ uint32_t option_bits );    
@@ -135,16 +135,16 @@ extern void matrix4_rotationf (
 
 /* copies In to column ColumnIndex of Out.*/
 extern void matrix_setColumnf (
-    /* out */ void *Out,
-    /* in */ void *In,
+    /* out notnull */ void *Out,
+    /* in notnull */ void *In,
     /* in */ uint32_t ColumnIndex,
     /* in */ uint32_t NumberOfRows,
     /* in */ uint32_t ColumnsPerRow );
 
 /* copies In to row RowIndex of Out.*/
 extern void matrix_setRowf (
-    /* out */ void *Out,
-    /* in */ void *In,
+    /* out notnull */ void *Out,
+    /* in notnull */ void *In,
     /* in */ uint32_t RowIndex,
     /* in */ uint32_t ColumnsPerRow );
 
@@ -160,15 +160,34 @@ extern void matrix_setRowf (
  *     single row of a matrix.
  * Weak contract: 
  *     - pvResultMatrix and pvGivenMatrix are both 
- *          non-null, non-corrupted memory containing 
+ *          non-null, memory containing 
  *          dwNumberOfRows*dwColumnsPerRow elements.
  *     - pvResultMatrix is stored in writable memory.
  *     - pvGivenMatrix is stored in readable memory.
  */
 extern void matrix_transposef ( 
-    /* out */ void *pvResultMatrix, 
-    /* in */ void *pvGivenMatrix, 
+    /* out notnull */ void *pvResultMatrix, 
+    /* in notnull */ void *pvGivenMatrix, 
     /* in */ uint32_t dwNumberOfRows, 
     /* in */ uint32_t dwColumnsPerRow );
+
+/** 
+ * @brief Fills each element of the matrix pvResultMatrix
+ *     with fFillValue.
+ * @param pvResultMatrix An address specifying
+ *     where to store the filled matrix.
+ * @param fFillValue The value to fill each element of 
+ *     pvResultMatrix.
+ * @param dwNumberOfRows The number of rows in the matrix.
+ * @param dwColumnsPerRow The number of columns on a 
+ *     single row of a matrix.
+ * Weak contract: 
+ *     - pvResultMatrix is stored in writable memory.
+ */
+extern void matrix_fillf (
+    /* out notnull */ void *pvResultMatrix, 
+    /* in */ float fFillValue, 
+    /* in */ uint32_t NumberOfRows, 
+    /* in */ uint32_t ColumnsPerRow ) 
 
 #endif /* __MATRIX_A6ACC912_1ED2_4C9A_82C4_1872183959E1 */
